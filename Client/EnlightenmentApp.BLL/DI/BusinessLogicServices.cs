905659866b379dca866f8ccd6a33f664d29c9d57
VadimZharikov <vadimzharikov2003@gmail.com>
2023-12-29 13:39:19 +0300

    add migrations
    add DbContext
    
    Add data entities

---
+++ Client/EnlightenmentApp.BLL/DI/BusinessLogicServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnlightenmentApp.BLL.DI
+{
+    public static class BusinessLogicServices
+    {
+        public static void AddBusinessLogic(this IServiceCollection services, IConfiguration configuration)
+        {
+            DAL.DI.DataAccessServices.AddDataRepository(services, configuration);
+        }
+    }
+}
---
+++ Client/EnlightenmentApp.DAL/Entities/TagEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace EnlightenmentApp.DAL.Entities
+{
+    public class TagEntity
+    {
+        public enum TagType
+        {
+            Tier,
+            Difficulty,
+            SectionsCount,
+            ReturnAmount,
+            Timespan,
+            Custom
+        }
+        #nullable disable
+        public int Id { get; set; }
+        public TagType Type { get; set; }
+        public string Value { get; set; }
+        public string MetaData { get; set; }
+        public bool IsBasic { get; set; }
+        public ICollection<ModuleEntity> Modules { get; set; }
+        public ICollection<PathEntity> Paths { get; set; }
+    }
+}
