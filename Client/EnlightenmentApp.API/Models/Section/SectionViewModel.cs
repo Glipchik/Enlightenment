16836a56a6f69db03b59c8bc2872429cc33f7e55
Вадим Жариков <vadzhar@mail.ru>
2024-01-05 12:40:41 +0300

    add id to section

--- Client/EnlightenmentApp.API/Models/Section/SectionViewModel.cs
+++ Client/EnlightenmentApp.API/Models/Section/SectionViewModel.cs
@@ -3,6 +3,7 @@
     public class SectionViewModel
     {
         #nullable disable
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public bool IsCompleted { get; set; }
