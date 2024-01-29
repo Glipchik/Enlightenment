cdc708340d8dae198d23895ebedc586f174afb4c
VadimZharikov <vadimzharikov2003@gmail.com>
2024-01-29 14:56:36 +0300

    create services

---
+++ EnlightenmentApp.FrontendApp/frontend-react/src/services/PathService.ts
@@ -0,0 +1,9 @@
+import Path from "../data/models/Path/Path";
+import GenericService from "./GenericService";
+
+export default class PathService extends GenericService<Path> {
+  constructor() {
+    super();
+    this.url = `${process.env.REACT_APP_ENLIGHTENMENT_API}/api/paths`;
+  }
+}
